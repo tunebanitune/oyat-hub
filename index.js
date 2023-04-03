3c70f62aa9e4575a5cd1f1a4cc4ed65bbb65fed4
tunebanitune <aln_bsh@yahoo.com>
2023-04-03 21:07:01 +0100

    transition to flutter 1

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 class Communication {
     constructor() {
         this.app = express();
         this.server = http.createServer(this.app);
-        this.io = new Server(this.server);
+        this.io = new Server(this.server, {
+            cors: {
+                origin: '*', //Replace '*' with domain to restrict the allowed origins
+                methods: ['GET', 'POST']
+            }
+        });
         this.configureExpress();
         this.configureSocketIO();
     }
 
     configureExpress() {
         console.log('##configuring express');
+        this.app.use(cors());
         this.app.use(express.static('public'));
         this.app.use('/communicationClient', express.static('public/communicationClient'));
         this.app.get('/socket.io/socket.io.js', (req, res) => {
